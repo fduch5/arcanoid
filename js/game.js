151033f3a5919304eb212ffd136bfd3202cdf422
damba <damba1010002@gmail.com>
2025-02-06 17:27:50 +0700

    5. Выводим блоки на экран 4x8

--- js/game.js
+++ js/game.js
@@ -2,10 +2,14 @@ let game = {
     cntx: null,
     platform: null,
     ball: null,
+    blocks: [],
+    rows: 4,
+    cols: 8,
     sprites: {
         background: null,
         ball: null,
-        platform: null
+        platform: null,
+        block: null
     },
 
     init(){
@@ -15,25 +19,21 @@ let game = {
     preload(callback){
         let loaded  = 0;
         const required = Object.keys(this.sprites).length;
+        let onImageLoad = () => {
+            loaded += 1;
+            if(loaded >= required){
+                callback();
+            }
+        }
         // отследить событие когда картинки будут загружены полностью каждый
         for(let key in this.sprites){
             this.sprites[key] = new Image();
             this.sprites[key].src = `./sprites/${key}.png`;
             // отслеживаем загрузку картин, уже после callback
-            this.sprites[key].addEventListener("load", () => {
-                loaded += 1;
-                if(loaded >= required){
-                    callback();
-                }
-            });
+            this.sprites[key].addEventListener("load", onImageLoad);
         }
     },
 
-    run(){
-        window.requestAnimationFrame(() => {
-            this.render();
-        });
-    },
     render(){
         this.cntx.drawImage(this.sprites.background, 0, 0);
         this.cntx.drawImage(this.sprites.ball,
@@ -42,17 +42,44 @@ let game = {
         );
         this.cntx.drawImage(this.sprites.platform, this.platform.x,
             this.platform.y);
+
+        this.renderBlocks();
     },
-    start: function(){
+
+    renderBlocks(){
+        for(let block of this.blocks){
+            this.cntx.drawImage(this.sprites.block, block.x, block.y);
+        }
+    },
+
+    create(){
+        for(let row = 0; row < this.rows; row += 1){
+            for(let col = 0; col < this.cols; col += 1){
+                this.blocks.push({
+                    x: 64 * col + 65,
+                    y: 22 * row + 35
+                });
+            }
+        }
+    },
+
+    run(){
+        window.requestAnimationFrame(() => {
+            this.render();
+        });
+    },
+
+    start(){
         this.init();
         this.preload(() => {
+            this.create();
             this.run();
         });
     },
 };
 
 game.platform = {
-    x: 620 / 2 - 40,
+    x: 620 / 2 - 32,
     y: 300
 };
 
