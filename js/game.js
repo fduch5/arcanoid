689f8a626d2b6a610bbe2c43e1d7c0289247df79
damba <damba1010002@gmail.com>
2025-02-06 12:43:54 +0700

    4. Расставляем объекты на экране, рисуем часть спрайта мяча

--- js/game.js
+++ js/game.js
@@ -1,10 +1,13 @@
 let game = {
     cntx: null,
+    platform: null,
+    ball: null,
     sprites: {
         background: null,
         ball: null,
         platform: null
     },
+
     init(){
         const gameCanvas = document.querySelector("#game-canvas");
         this.cntx = gameCanvas.getContext("2d");
@@ -33,8 +36,12 @@ let game = {
     },
     render(){
         this.cntx.drawImage(this.sprites.background, 0, 0);
-        this.cntx.drawImage(this.sprites.ball, 0, 0);
-        this.cntx.drawImage(this.sprites.platform, 100, 100);
+        this.cntx.drawImage(this.sprites.ball,
+            0, 0, this.ball.width, this.ball.height,
+            this.ball.x, this.ball.y, this.ball.width, this.ball.height
+        );
+        this.cntx.drawImage(this.sprites.platform, this.platform.x,
+            this.platform.y);
     },
     start: function(){
         this.init();
@@ -42,6 +49,18 @@ let game = {
             this.run();
         });
     },
+};
+
+game.platform = {
+    x: 620 / 2 - 40,
+    y: 300
+};
+
+game.ball = {
+    x: 320,
+    y: 280,
+    width: 20,
+    height: 20,
 };
 
 window.addEventListener('load', () => {
